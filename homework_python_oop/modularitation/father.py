5df6cd1a230c70d434ef7a4c28c57455ebb303b6
Maia Rojas <maia.rt.46@gmail.com>
2023-02-07 00:06:43 -0500

    Add exercise

---
+++ homework_python_oop/modularitation/father.py
@@ -0,0 +1,21 @@
+local_val = "unicornios mágicos"
+def square(x):
+  return x * x
+class User:
+  def __init__(self, name):
+    self.name = name
+  def di_hola(self):
+    return "hola"
+
+# en el mismo archivo, agrega lo siguiente debajo de la clase Usuario
+print(square(5))
+user = User("Anna")
+print(user.name)
+print(user.di_hola())
+
+print(__name__)
+
+if __name__ == "__name__":
+  print('El archivo se esta ejecutando directamente')
+else:
+  print('El archivo se esta ejecutando porque es importado por otro archivo. El archivo se llama,', __name__)
