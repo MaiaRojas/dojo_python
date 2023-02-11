0b1fc2c6868ca6e795762ecd2382848ff622a9b8
Maia Rojas <maia.rt.46@gmail.com>
2023-02-11 15:36:58 -0500

    Add Math dojo

---
+++ homework_python_oop/dojo_pet/math_dojo.py
@@ -0,0 +1,20 @@
+class MathDojo:
+  def __init__(self):
+    self.result = 0
+  def add(self, num, *nums):
+    self.result += num
+    for a in nums:
+      self.result += a
+    return self
+  def subtract(self, num, *nums):
+    self.result -= num
+    for a in nums:
+      self.result -= a
+    return self
+    
+# crear una instancia:
+md = MathDojo()
+# para probar:
+x = md.add(2).add(2,5,1).subtract(3,2).result
+print(x)	# debería imprimir 5
+# ejecuta cada uno de los métodos unas cuantas veces más y verifica el resultado
